aa229ba10102fec8c1b31c3b954266ae6759fdd0
TobiLayderer93 <kugelbergerer@gmail.com>
2021-06-16 11:08:27 +0200

    wip

--- sample-pipe.py
+++ sample-pipe.py
@@ -1,9 +1,4 @@
-
-import kfp
 from kfp import dsl
-import subprocess
-import os
-import time
 from kfp.v2 import compiler, components
 from kfp.v2.dsl import (
     component,
@@ -16,32 +11,9 @@ from kfp.v2.google.client import AIPlatformClient
 from google.cloud import aiplatform
 from google_cloud_pipeline_components import aiplatform as gcc_aip
 
-'''
-def gcs_download_op(url):
-    return dsl.ContainerOp(
-        name='GCS - Download',
-        image='google/cloud-sdk:279.0.0',
-        command=['sh', '-c'],
-        arguments=['gsutil cat $0 | tee $1', url, '/tmp/results.txt'],
-        file_outputs={
-            'data': '/tmp/results.txt',
-        }
-    )
-
-
-def echo_op(text):
-    return dsl.ContainerOp(
-        name='echo',
-        image='library/bash:4.4.23',
-        command=['sh', '-c'],
-        arguments=['echo "$0"', text]
-    )
-
-'''
-
 
 @component(packages_to_install=['google-api-python-client'])
-def bq_sample() -> str:
+def run_bq_query_job(project_id: str, query: str) -> str:
     import subprocess
 
     subprocess.run(["pip", "install", "google-api-python-client"])
@@ -49,31 +21,30 @@ def bq_sample() -> str:
 
     client = googleapiclient.discovery.build('bigquery', 'v2')
 
-    query = '''
-        DELETE FROM `spielwiese-tobias.test.my_new_table` WHERE manufacturer = "klickTel AG"
-    '''
+    query = query
     request_body = {
         "query": query,
         "useLegacySql": False
     }
-
     result = None
     try:
         result = client.jobs().query(
-            projectId='spielwiese-tobias',
+            projectId=project_id,
             body=request_body
         ).execute()
     except Exception as ex:
-        raise Exception("my test error: {}".format(ex))
+        raise Exception("Error starting BQ query job: {}".format(ex))
 
     if result:
         return result.get("jobReference").get("jobId")
     else:
-        return 'failiure'
+        return 'Failed getting job ID for query job'
 
 
 @component(packages_to_install=['google-api-python-client'])
-def wait_for_job_status(job_id: str) -> str:
+def wait_for_job_status(job_id: str,
+                        project_id: str,
+                        timeout_s: int) -> str:
     import subprocess
     import time
 
@@ -94,11 +65,12 @@ def wait_for_job_status(job_id: str) -> str:
 
     start_time = time.time()
     while True:
-        status = __get_job_status(project_id='spielwiese-tobias', job_id=job_id)
+        status = __get_job_status(project_id=project_id, job_id=job_id)
         if status == "PENDING" or status == "RUNNING":
             elapsed_time = time.time() - start_time
-            if elapsed_time > 900:
-                return "Waiting for BigQuery job with job_id={0}. Timeout. Waited time={1}s. Job status={2}".format(job_id,round(elapsed_time,2),status)
+            if elapsed_time > timeout_s:
+                return "Waiting for BigQuery job with job_id={0}. Timeout. Waited time={1}s. Job status={2}"\
+                    .format(job_id, round(elapsed_time, 2), status)
             time.sleep(5)
         else:
             break
@@ -106,47 +78,146 @@ def wait_for_job_status(job_id: str) -> str:
     return 'success'
 
 
-def add(a: float, b: float) -> float:
-  '''Calculates sum of two arguments'''
-  return a + b
+@component(packages_to_install=['google-api-python-client'])
+def launch_dataflow_flex_template(result: str) -> str:
+    import subprocess
+
+    subprocess.run(["pip", "install", "google-api-python-client"])
+    import googleapiclient.discovery
+
+    client = googleapiclient.discovery.build('dataflow', 'v1b3')
+
+    result = client.projects().locations().flexTemplates().launch(
+        projectId='vortex-sample',
+        location='europe-west1',
+        body={"launchParameter": {
+            "jobName": "sample",
+            "parameters": {
+                "tableRef": "spielwiese-tobias:test.my_new_table",
+                "bucket": "gs://vertex-gcs",
+                "numShards": "10",
+                "fields": "product_id,ean,subsidiary"
+            },
+            "containerSpecGcsPath": "gs://dataflow-templates/latest/flex/BigQuery_to_Parquet"
+        }}
+    ).execute()
+
+    return result.get("job").get("id")
+
+
+@component(packages_to_install=['google-api-python-client'])
+def launch_dataflow_template(job_name: str,
+                             project_id: str,
+                             num_workers: int,
+                             max_workers: int,
+                             location: str,
+                             temp_location: str,
+                             machine_type: str,
+                             template_path: str,
+                             params: dict) -> str:
+    import subprocess
+
+    subprocess.run(["pip", "install", "google-api-python-client"])
+    import googleapiclient.discovery
+
+    client = googleapiclient.discovery.build('dataflow', 'v1b3')
+
+    result = None
+    try:
+        result = client.projects().templates().launch(
+            projectId=project_id,
+            location=location,
+            gcsPath=template_path,
+            body={
+                "jobName": job_name,
+                "parameters": params,
+                "environment":{
+                    "numWorkers": num_workers,
+                    "maxWorkers": max_workers,
+                    "tempLocation": temp_location,
+                    "machineType": machine_type
+                }
+            }
+        ).execute()
+    except Exception as ex:
+        raise Exception('Error launching dataflow template: {}'.format(ex))
+
+    if result:
+        return result.get("job").get("id")
+    else:
+        raise Exception('No result launching dataflow template')
+
+
+@component(packages_to_install=['google-api-python-client'])
+def wait_for_dataflow_job_to_finish(job_id: str,
+                                    project_id: str,
+                                    location: str,
+                                    timeout_s: int) -> str:
+    import subprocess
+    import time
+
+    subprocess.run(["pip", "install", "google-api-python-client"])
+    import googleapiclient.discovery
+
+    def __get_job_status(project_id: str, job_id: str, location: str) -> str:
+        client = googleapiclient.discovery.build('dataflow', 'v1b3')
+
+        result = None
+        try:
+            result = client.projects().locations().jobs().get(
+                projectId=project_id,
+                location=location,
+                jobId=job_id
+            ).execute()
+        except Exception as ex:
+            raise Exception('Failed to get job status for id: {} with Error: {}'.format(job_id, ex))
+
+        if result:
+            return result.get("currentState")
+        else:
+            raise Exception('Failed getting result for job id: {}'.format(job_id))
+
+    start_time = time.time()
+    while True:
+        status = __get_job_status(project_id=project_id, job_id=job_id, location=location)
+        if status != "JOB_STATE_DONE":
+            elapsed_time = time.time() - start_time
+            if elapsed_time > timeout_s:
+                return "Waiting for Dataflow job with job_id={0}. Timeout. Waited time={1}s. Job status={2}".format(job_id,round(elapsed_time,2),status)
+            time.sleep(60)
+        else:
+            break
+
+    return 'success'
 
 
 
 @dsl.pipeline(
-  name='bq-pipeline',
+  name='batch-reload',
   description='An example pipeline that performs addition calculations.',
 )
-def add_pipeline():
+def batch_reload_pipeline(project_id_bq: str, query: str, project_id: str, location: str):
 
-    # Passes a pipeline parameter and a constant value to the `add` factory
-    # function.
-    job_id = bq_sample()
-    # Passes an output reference from `first_add_task` and a pipeline parameter
-    # to the `add` factory function. For operations with a single return
-    # value, the output reference can be accessed as `task.output` or
-    # `task.outputs['output_name']`.
-    output = wait_for_job_status(job_id.output)
+    # Start BQ query job and receive job id
+    bq_job_id = run_bq_query_job(project_id=project_id_bq, query=query)
 
-# Specify pipeline argument values
+    # Wait for BQ to finish
+    result = wait_for_job_status(job_id=bq_job_id.output, project_id=project_id_bq, timeout_s=900)
+
+    # launch dataflow template
+    dataflow_job_id = launch_dataflow_flex_template(result= result.output)
+
+    # wait for datflow pipe to finish
+    result_datflow = wait_for_dataflow_job_to_finish(job_id=dataflow_job_id.output, project_id=project_id, location=location, timeout_s=3600)
+
 
 if __name__ == '__main__':
-    #client = kfp.Client(host='https://1f4b406cc9338a6d-dot-europe-west1.pipelines.googleusercontent.com')
-
-    '''
-    client.create_run_from_pipeline_func(
-        sequential_pipeline,
-        arguments={
-            'url': 'gs://gcs-sample-th/th-test/sample.txt'
-        })
-    '''
 
     project_id = 'vortex-sample'
     region = 'us-central1'
     pipeline_root_path = 'gs://vertex-gcs'
 
-    #kfp.compiler.Compiler().compile(sequential_pipeline, __file__ + '.yaml')
-
-    compiler.Compiler().compile(pipeline_func=add_pipeline,
+    compiler.Compiler().compile(pipeline_func=batch_reload_pipeline,
                                 package_path='sample-pipe-desc.json'
                             )
 
@@ -154,7 +225,12 @@ if __name__ == '__main__':
 
     response = api_client.create_run_from_job_spec(
         'sample-pipe-desc.json',
-        pipeline_root=pipeline_root_path
+        pipeline_root=pipeline_root_path,
+        parameter_values={'project_id_bq': 'spielwiese-tobias',
+                          'query': '''DELETE FROM `spielwiese-tobias.test.my_new_table` where manufacturer = "VIVANCO"''',
+                          'project_id': 'vortex-sample',
+                          'location': 'europe-west1'
+                          }
     )
 
 
--- temp.py
+++ temp.py
@@ -1,24 +1,39 @@
 
 
+def launch(client):
+    result = client.projects().locations().flexTemplates().launch(
+        projectId='vortex-sample',
+        location='europe-west1',
+        body={"launchParameter": {
+            "jobName": "sample",
+            "parameters": {
+                "tableRef": "spielwiese-tobias:test.my_new_table",
+                "bucket": "gs://vertex-gcs",
+                "numShards": "10",
+                "fields": "product_id,ean,subsidiary"
+            },
+            "containerSpecGcsPath": "gs://dataflow-templates/latest/flex/BigQuery_to_Parquet"
+        }}
+    ).execute()
+    #result.get("job").get("id")
+    print(result)
+
+def get_dataflow_job(job_id):
+
+    result = client.projects().locations().jobs().get(
+        projectId='vortex-sample',
+        location='europe-west1',
+        jobId=job_id
+    ).execute()
+    #result.get("currentState")
+    print(result)
+
 if __name__ == '__main__':
     import googleapiclient.discovery
 
-    client = googleapiclient.discovery.build('bigquery', 'v2')
+    client = googleapiclient.discovery.build('dataflow', 'v1b3')
 
-    query = '''
-            DELETE FROM `spielwiese-tobias.test.my_new_table` WHERE manufacturer = "GORENJE"
-        '''
-    request_body = {
-        "query": query,
-        "useLegacySql": False
-    }
+    #launch(client)
+    get_dataflow_job('2021-06-16_01_03_36-17460218073891160619')
 
-    try:
-        result = client.jobs().query(
-            projectId='spielwiese-tobias',
-            body=request_body
-        ).execute()
-    except Exception as ex:
-        print('error')
-
-    print(result)
+    #2021-06-16_00_42_00-11764626317786107316
